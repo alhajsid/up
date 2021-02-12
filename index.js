06c5249d917964e8925aab24ca0b7c97a1a722aa
Mohd Alhaj <dev.alhaj@gmail.com>
2021-02-12 10:38:56 +0530

    added new

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var url = require('url');
+var AWS = require('aws-sdk');
 
 var fs = require('fs');
 const express = require('express')
@@ -14,5 +15,25 @@ app.get('/devil', function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello devil')
 })
+
+AWS.config.update({ 
+    accessKeyId: ':)))',
+    secretAccessKey: ':DDDD',
+    region: 'ap-south-1',
+    signatureVersion: 'v4'
+});
+
+const s3 = new AWS.S3()
+const myBucket = ':)))))'
+const myKey = ':DDDDDD'
+const signedUrlExpireSeconds = 60 * 5
+
+const url = s3.getSignedUrl('getObject', {
+    Bucket: myBucket,
+    Key: myKey,
+    Expires: signedUrlExpireSeconds
+});
+
+console.log(url);
 
 app.listen(8080)
